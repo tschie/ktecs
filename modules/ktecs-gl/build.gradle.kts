e604f8bba276254e1fe300d03d77401d3d07896c
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-10-18 22:30:09 -0700

    add dokka plugin

--- modules/ktecs-gl/build.gradle.kts
+++ modules/ktecs-gl/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.4.10"
+    id("org.jetbrains.dokka") version "1.4.10"
     id("maven-publish")
 }
 group = "io.github.tschie.ktecs"
@@ -7,6 +8,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
     mavenLocal()
 }
 kotlin {
