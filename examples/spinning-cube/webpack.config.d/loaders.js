10a12ac4c80c5ef6889a2e0eb3910dce51ae9450
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-10-19 13:32:22 -0700

    Delete loaders.js

--- examples/spinning-cube/webpack.config.d/loaders.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const nodeModulesPath = path.resolve(__dirname, "node_modules");
-
-config.module.rules.push({
-    test: /\.(glsl|frag|vert)$/,
-    loader: ["raw-loader", "glslify-loader"],
-    exclude: [nodeModulesPath],
-});
