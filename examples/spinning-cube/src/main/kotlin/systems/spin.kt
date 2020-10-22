94a27782849e281beaf6df154e4102cee965b1fb
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-10-22 11:42:28 -0700

    radians to degrees

--- examples/spinning-cube/src/main/kotlin/systems/spin.kt
+++ examples/spinning-cube/src/main/kotlin/systems/spin.kt
@@ -2,21 +2,20 @@ package systems
 
 import components.Spin
 import components.spin
-import io.github.tschie.ktecs.core.World
-import io.github.tschie.ktecs.core.and
-import io.github.tschie.ktecs.core.has
+import io.github.tschie.ktecs.core.query.and
+import io.github.tschie.ktecs.core.query.has
+import io.github.tschie.ktecs.core.world.World
 import io.github.tschie.ktecs.gl.components.Transform
 import io.github.tschie.ktecs.gl.components.transform
 import io.github.tschie.ktecs.gl.math.plus
 import io.github.tschie.ktecs.gl.math.rem
 import io.github.tschie.ktecs.gl.math.times
-import kotlin.math.PI
 
 fun World.spin(delta: Double) {
   query(has<Transform>() and has<Spin>()).forEach { entity ->
     val spin = entity.spin!!
     val transform = entity.transform!!
-    val rotation = (transform.rotation + spin.value * delta.toFloat()) % (2f * PI.toFloat())
+    val rotation = transform.rotation + (spin.value * delta.toFloat()) % 360f
     entity.transform = transform.copy(rotation = rotation)
   }
 }
