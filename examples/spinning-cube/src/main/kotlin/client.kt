37291675f9e49351d234b2622c154859f060d57b
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-10-22 11:41:48 -0700

    clean up

--- examples/spinning-cube/src/main/kotlin/client.kt
+++ examples/spinning-cube/src/main/kotlin/client.kt
@@ -6,12 +6,11 @@ import data.cubeData
 import data.fragmentShaderSource
 import data.randomColors
 import data.vertexShaderSource
-import io.github.tschie.ktecs.core.Entity
-import io.github.tschie.ktecs.core.World
-import io.github.tschie.ktecs.core.entity
-import io.github.tschie.ktecs.core.world
+import io.github.tschie.ktecs.core.entity.entity
+import io.github.tschie.ktecs.core.world.World
+import io.github.tschie.ktecs.core.world.world
 import io.github.tschie.ktecs.gl.components.*
-import io.github.tschie.ktecs.gl.math.Vec3f
+import io.github.tschie.ktecs.gl.math.*
 import io.github.tschie.ktecs.gl.systems.WebGLRenderer
 import io.github.tschie.ktecs.gl.systems.render
 import kotlinx.browser.document
@@ -23,7 +22,6 @@ import systems.spin
 
 lateinit var sandbox: World
 lateinit var webGLRenderer: WebGLRenderer
-lateinit var cube: Entity
 
 fun main() {
   val canvas = document.querySelector("canvas") as HTMLCanvasElement
@@ -46,20 +44,21 @@ fun main() {
       }
     }
 
-    cube = entity {
+    // cube
+    entity {
       transform = Transform(position = Vec3f(z = -10.0f))
       mesh = Mesh(mapOf("position" to cubeData, "color" to randomColors(6, 3)))
       material = Material(vertexShaderSource, fragmentShaderSource)
-      spin = Spin(value = Vec3f(0.001f, 0.001f, 0.001f))
+      spin = Spin(value = Vec3f(xyz = 0.003f))
     }
   }
 
   webGLRenderer = WebGLRenderer(gl)
 
-  animate(0.0, window.performance.now())
+  animate()
 }
 
-fun animate(delta: Double, time: Double) {
+fun animate(delta: Double = 0.0, time: Double = window.performance.now()) {
   with (sandbox) {
     spin(delta)
     render(webGLRenderer)
