5c1f885499e3406c45b1b40958705ddde251f349
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-10-22 11:38:14 -0700

    publishing and doc plugin config

---
+++ modules/ktecs-core/README.md
@@ -0,0 +1,1 @@
+# ktecs-core
--- modules/ktecs-core/build.gradle.kts
+++ modules/ktecs-core/build.gradle.kts
@@ -1,10 +1,25 @@
 plugins {
     kotlin("multiplatform") version "1.4.10"
-    id("org.jetbrains.dokka") version "1.4.10"
     id("maven-publish")
 }
+
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.10.2")
+    }
+}
+
+if (!project.plugins.hasPlugin("org.jetbrains.dokka")) {
+    project.plugins.apply("org.jetbrains.dokka")
+}
+
 group = "io.github.tschie.ktecs"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -23,11 +38,7 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-            }
-        }
+        val commonMain by getting
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -38,6 +49,19 @@ kotlin {
         val jsTest by getting {
             dependencies {
                 implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/tschie/ktecs")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
         }
     }
