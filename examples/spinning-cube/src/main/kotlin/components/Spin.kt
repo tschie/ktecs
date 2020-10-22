6ced27f5ca903239c9d0ebc924bd78d81cac4754
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-10-22 11:42:20 -0700

    fix packages

--- examples/spinning-cube/src/main/kotlin/components/Spin.kt
+++ examples/spinning-cube/src/main/kotlin/components/Spin.kt
@@ -1,8 +1,8 @@
 package components
 
-import io.github.tschie.ktecs.core.Component
 import io.github.tschie.ktecs.core.Ktecs
-import io.github.tschie.ktecs.core.Entity
+import io.github.tschie.ktecs.core.component.Component
+import io.github.tschie.ktecs.core.entity.Entity
 import io.github.tschie.ktecs.gl.math.Vec3f
 
 data class Spin(val value: Vec3f = Vec3f())
